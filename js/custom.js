92d3bb7d65dcbb5fc17a8d6bf1e1f8063b469a64
Kavan Thakkar <95115101+Ka1Thakkar@users.noreply.github.com>
2024-06-21 10:02:58 +0530

    Basic copy and UI changes

--- js/custom.js
+++ js/custom.js
@@ -36,8 +36,9 @@ $('.chocolate_container').slick({
 /** google_map js **/
 
 function myMap() {
+    
     var mapProp = {
-        center: new google.maps.LatLng(40.712775, -74.005973),
+        center: new google.maps.LatLng(12.971599, 77.594566),
         zoom: 18,
     };
     var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
